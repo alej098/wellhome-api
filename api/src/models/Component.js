fc71e204735bedf81883a35d8cda5881fca070bf
alcazarpierre <alcazarpierre@gmail.com>
2024-02-09 21:47:01 -0500

    solucionado el modelo de component

--- api/src/models/Component.js
+++ api/src/models/Component.js
@@ -25,7 +25,7 @@ module.exports = (sequelize) => {
             allowNull: false,
         },
         acceptCost: {
-            type: DataTypes.STRING,
+            type: DataTypes.BOOLEAN,
             defaultValue: true,
         },
         isSuspended: {
