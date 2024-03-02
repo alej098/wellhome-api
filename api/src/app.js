837f06aa47c4956bc5afa8e169c406391edb9180
alcazarpierre <alcazarpierre@gmail.com>
2024-03-02 18:30:56 -0500

    localhost directo

--- api/src/app.js
+++ api/src/app.js
@@ -19,7 +19,7 @@ app.use(morgan('dev'));
 
 // Middleware para configurar CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${FRONT_DOMAIN}`); 
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4000'); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
